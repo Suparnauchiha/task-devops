3fedf38d05f897835a6e458538ca820e65c1da0a
Suparnauchiha <suparnaurmipaul2004@gmail.com>
2026-08-05 01:12:21 +0530

    msg

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log(' Shutting down gracefully...');
   await mongoose.connection.close();
   process.exit(0);
 });
